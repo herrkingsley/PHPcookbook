7da7279876f1b2bfaadcceb86bbb95dea1aac9ce
Jonas Carboo <jonas.carboo@pantgbg.se>
2020-08-21 17:50:23 +0200

    Made addIngredient method

--- recipie.php
+++ recipie.php
@@ -8,6 +8,20 @@ class Recipie
     public $tag = array();
     public $source = "Jonas Carboo";
 
+    private $measurements = array(
+        "kryddmått",
+        "tsk",
+        "msk",
+        "mg",
+        "g",
+        "hg",
+        "kg",
+        "ml",
+        "cl",
+        "dl",
+        "st"  
+    );
+
     
     public function setTitle($title){  // Setter
         $this->title = ucwords($title);
@@ -17,12 +31,20 @@ class Recipie
         return $this->title;
     }
     // set variables to null let's me have ingredients like egg without measurement.
-    public function addInredient($item, $amount = null, $measure = null){
-        $this->ingredient[] = array(
-            "item" => ucwords($item),
-            "amount" => $amount,
-            "measure" => $measure
-        );
+    public function addIngredient($item, $amount = null, $measure = null){
+        
+        if($amount != null && !is_float($amount) && !is_int($amount) ){
+            exit("Antal måste vara en siffra");
+        }
+        if($measure != null && !in_array($measure, $this->measurements)){
+            exit("Mått måste vara nåt av följande mått: " . implode(", ",$this->measurements));
+        }
+            $this->ingredients[] = array(
+                "item" => $item,
+                "amount" => $amount,
+                "measure" => $measure
+            );
+  
     }
 
     public function displayRecipie() {
@@ -32,7 +54,6 @@ class Recipie
 
 $recipie1 = new Recipie();
 $recipie1->source = "Jonas Carboo";
-$recipie1->setTitle("köttfärssås");
-echo $recipie1->getTitle() . "<br>";
+$recipie1->addIngredient("Ägg");
 
 echo $recipie1->displayRecipie();
